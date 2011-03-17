27b0fe90bcfe23f785ff122dc9f0338a69b3816d
chandru <chandru@xtrafin.net>
2011-03-17 18:13:35 +0530

    some changes on added transaction method on after create

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -3,17 +3,20 @@ class Entry < ActiveRecord::Base
   belongs_to :transaction
 
   after_create :transac
-
+ # after_commit :transaction1
+  
   def transac
-    @cash_balance = Account.find_by_id(1)
-    @inventory_balance = Account.find_by_id(2)
-    if self.account_id == 1 && self.transaction_type == "Credit"
-      @cash_balance.update_attributes(:balance => (@cash_balance.balance - self.amount))
-      @cash_balance.save!
-    elsif self.account_id == 2 && self.transaction_type == "Debit"
-      @inventory_balance.update_attributes(:balance => (@inventory_balance.balance + self.amount))
-      @inventory_balance.save!
+   Entry.transaction do 
+      @cash_balance = Account.find_by_id(1)
+      @inventory_balance = Account.find_by_id(2)
+      if self.account_id == 1 && self.transaction_type == "Credit"
+        @cash_balance.update_attributes(:balance => (@cash_balance.balance - self.amount))
+        @cash_balance.save!
+      elsif self.account_id == 2 && self.transaction_type == "Debit"
+        @inventory_balance.update_attributes(:balance => (@inventory_balance.balance + self.amount))
+        @inventory_balance.save!
+      end
+      save!
     end
-    #save!
   end
 end
